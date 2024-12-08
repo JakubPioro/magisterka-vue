e03a37e6b2fdd1008dcd7d6b2eedb0567a5ee3d1
JakubPioro <64768613+JakubPioro@users.noreply.github.com>
2024-12-09 00:21:39 +0100

    main page

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+const title = "main page";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>
+      {{title}}
+    </h1>
   </main>
 </template>
