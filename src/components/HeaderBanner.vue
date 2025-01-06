ff269cc471f93f135d5cd505b4e27959851f9de2
JakubPioro <64768613+JakubPioro@users.noreply.github.com>
2025-01-07 00:25:40 +0100

    added HeaderBanner.vue

---
+++ src/components/HeaderBanner.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="">
+  <p class="text-9xl text-center bg-[url('/header-background.png')] bg-clip-text text-transparent whitespace-pre-line m-8 text-shadow-sm">promotion hunter</p>
+</div>
+</template>
+
