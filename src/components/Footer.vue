c23bcc3ee60858f1a8b3024ab2b820df4f3b61c7
JakubPioro <64768613+JakubPioro@users.noreply.github.com>
2025-01-07 00:27:04 +0100

    added Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex bg-black gap-2 w-full justify-center">
+    <div class="flex flex-col p-8 gap-4 max-w-[1200px]">
+      <div class="text-4xl text-white">We’re not like the others – our team cares about your finances, bringing you handpicked deals and honest offers. We believe everyone deserves the best products at the best prices. Thank you for choosing us – together, we save smart!</div>
+      <div class="text-lg text-wrap">Without you, we wouldn’t be here – your trust and support inspire everything we do. Here’s what we’ve achieved together:</div>
+      <div class="flex gap-32">
+        <div class="flex flex-col">
+          <p class="text-6xl text-purple-theme">25000+</p>
+          <p class="">Clients</p>
+        </div>
+        <div class="flex flex-col">
+          <p class="text-6xl text-purple-theme">5000+</p>
+          <p class="">Orders daily</p>
+        </div>
+        <div class="flex flex-col">
+          <p class="text-6xl text-purple-theme">100+</p>
+          <p class="">New promotions weakly</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col p-8 min-w-fit gap-4 items-center">
+      <div class="flex">
+        <img
+          src="/avatar1.webp"
+          alt="avatar1"
+          class="rounded-full border-4 border-purple-theme w-16 h-16"
+        />
+        <img
+          src="/avatar2.png"
+          alt="avatar1"
+          class="rounded-full border-4 border-purple-theme w-16 h-16"
+        />
+        <img
+          src="/avatar3.jpeg"
+          alt="avatar1"
+          class="rounded-full border-4 border-purple-theme w-16 h-16"
+        />
+      </div>
+      <div class="bg-gray-800 py-1 px-4 text-center rounded-2xl w-fit text-white">Our team!</div>
+    </div>
+  </div>
+</template>
+
